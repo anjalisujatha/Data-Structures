a238c40adf48d643a00c97670ee004aab2daa4c5
anjalisujatha <anjalisujatha28@gmail.com>
2024-02-25 14:29:03 -0800

    Generated binary strings

---
+++ Recursion/generate_binary_strings.py
@@ -0,0 +1,17 @@
+def get_binary_strings(n):
+    """
+    Args:
+     n(int32)
+    Returns:
+     list_str
+    """
+
+    result = []
+    bshelper("", n, result)
+    return result
+def bshelper(slate, n, result):
+    if n == 0:
+       result.append(slate)
+    else:
+        bshelper(slate + "0", n-1, result)
+        bshelper(slate + "1", n-1, result)
