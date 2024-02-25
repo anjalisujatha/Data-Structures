5ba11c8b07f780ccbd050b5b68783ce586ae296b
anjalisujatha <anjalisujatha28@gmail.com>
2024-02-25 11:51:03 -0800

    file updated

---
+++ Recursion/tower_of_hanoi.py
@@ -0,0 +1,19 @@
+def tower_of_hanoi(n):
+    """
+    Args:
+     n(int32)
+    Returns:
+     list_list_int32
+    """
+    # Write your code here.
+    result = []
+    def transfer_disk(n, source, destination, auxiliary):
+        if n == 1:
+            result.append([source, destination])
+        else:
+            transfer_disk(n-1, source, auxiliary, destination)
+            result.append([source, destination])
+            transfer_disk(n-1, auxiliary, destination, source)
+            
+    transfer_disk(n, 1, 3, 2)
+    return result
